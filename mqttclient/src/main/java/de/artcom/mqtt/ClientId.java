28881fc2340b5d780f18d6bb575880411526b8cc
Mirko Geissler <mirko.geissler@artcom.de>
2017-12-21 15:33:10 +0100

    Move ClientId

--- mqttclient/src/main/java/de/artcom/mqtt/ClientId.java
+++ mqttclient/src/main/java/de/artcom/mqtt/ClientId.java
@@ -1,10 +1,9 @@
-package de.artcom.mqtt.id;
+package de.artcom.mqtt;
 
 
 import java.util.UUID;
 
 public class MqttClientId {
-
     public static String generate(String applicationId, String deviceId) {
         return applicationId + "-" + deviceId + "-" + UUID.randomUUID().toString().substring(0, 8);
     }
